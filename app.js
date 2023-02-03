0a19e285bbcdc4e8ebabd2a147a876f580781703
Jackson Rico <jackson.rico@deckard.com>
2023-02-03 05:27:30 -0500

    Update changes

--- app.js
+++ app.js
@@ -13,9 +13,11 @@ app.listen(3010, () =>{
 app.get('/', (req, res)=> {
     res.sendFile(path.resolve(__dirname, './views/index.html'))
 });
+
 app.get('/register', (req, res)=> {
     res.sendFile(path.resolve(__dirname, './views/register.html'))
 });
+
 app.get('/login', (req, res)=> {
     res.sendFile(path.resolve(__dirname, './views/login.html'))
 });
