dbdcfe116ef85194917ae471db20048601871a32
Jackson Rico <jackson.rico@deckard.com>
2023-02-04 01:13:06 -0500

    next change

--- app.js
+++ app.js
@@ -9,15 +9,15 @@ const publicFolderPath = path.resolve(__dirname, './public');
 app.use(express.static(publicFolderPath));
 
 app.get('/', (req, res)=> {
-    res.sendFile(path.resolve(__dirname, './src/views/index.html'))
+    res.sendFile(path.join(__dirname, './src/views/index.html'))
 });
 
 app.get('/register', (req, res)=> {
-    res.sendFile(path.resolve(__dirname, './src/views/register.html'))
+    res.sendFile(path.join(__dirname, './src/views/register.html'))
 });
 
 app.get('/login', (req, res)=> {
-    res.sendFile(path.resolve(__dirname, './src/views/login.html'))
+    res.sendFile(path.join(__dirname, './src/views/login.html'))
 });
 
 app.listen(port, () =>{
